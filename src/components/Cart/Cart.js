cbb7c4a6132adf34c58be621d40ccb9233bc4a1c
MDABDULLAH16 <mdabdullah161036@gmail.com>
2022-11-04 23:02:49 +0600

    localStorage done with set item

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -2,11 +2,26 @@ import React from 'react';
 import './Cart.css'
 
 const Cart = ({cart}) => {
+    let total = 0;
+    let shipping =0;
+    let quantity = 0;
+    for (const product of cart ){
+        quantity = quantity + product.quantity;
+         total = total + product.price * product.quantity;
+         shipping = shipping + product.shipping;
+    }
+        const tax = parseFloat((total* 0.1).toFixed(2));
+    const grandTotal = total + shipping + tax;
+
     return (
-        <div >
+        <div className='order-containers'>
+           <div className='cart-info'>
            <h4 className='order-text'>Order Summary </h4>
-           <div>
-           <p><small>Cart: {cart.length}</small></p>
+           <p>Selected Items: {quantity}</p>
+           <p>Total Price: ${total} </p>
+           <p>Total Shipping: ${shipping} </p>
+           <p>Tex: {tax}  </p>
+           <h4>Grand Total: {grandTotal}</h4>
            </div>
         </div>
     );
